127b8e7ecbb899c3b3c4038f2057a06ed4b5f963
Mohit Singh Tomar <mohitsingh2004624@gmail.com>
2025-08-22 13:23:34 +0530

    all stepup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { response } from 'express';
+import fs from "fs"
+
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+    const uploadOnCloudinary = async (locaFilePath) =>{
+        try {
+            if(!locaFilePath) return null
+            cloudinary.uploader.upload(locaFilePath,{
+                resource_type:"auto"
+            })
+
+            console.log("file is uploaded on cloudinary", response.url);
+            return response;
+        } catch (error) {
+            fs.unlinkSync(locaFilePath)
+
+            return null
+        }
+    }
+
+export {uploadOnCloudinary}
